0e3ae24f295c2889d04f02ccfa6ca7f98f86f8c8
Andreas Klostermann <andreas.klostermann@gmail.com>
2021-12-31 15:03:17 +0100

    further fixes

--- djangocms_text_mediumeditor/widgets.py
+++ djangocms_text_mediumeditor/widgets.py
@@ -10,21 +10,6 @@ from django.utils.safestring import mark_safe
 
 
 class MediumEditorWidget(Textarea):
-
+    template_name = 'cms/plugins/widgets/mediumeditor.html'
     def __init__(self, attrs=None):
         super(Textarea, self).__init__(attrs)
-
-    def render(self, name, value, attrs=None, renderer=None):
-        if attrs and "id" in attrs:
-            editor_id = attrs["id"]
-        else:
-            if not attrs:
-                attrs = {}
-            editor_id = attrs["id"] = "mediumeditor_%s" % (uuid.uuid4(),)
-
-        context = {"editor_id": editor_id}
-
-        return (
-            super(MediumEditorWidget, self).render(name, value, attrs)
-            + mark_safe(render_to_string("cms/plugins/widgets/mediumeditor.html", context))
-        )
